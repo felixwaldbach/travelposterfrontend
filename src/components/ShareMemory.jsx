7e2b48009a9df2b73c44b598d4fac95413998dd2
Marius Kießling <github@mariuskiessling.de>
2018-05-13 18:08:25 +0200

    Add image preview in upload section

--- src/components/ShareMemory.jsx
+++ src/components/ShareMemory.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone'
 
 import '../css/main.css';
 
@@ -8,8 +9,24 @@ class ShareMemory extends Component {
 
     this.state = {
       name: "",
-      file: ""
+      fileAsDataURL: ""
     }
+  }
+
+  _onDrop(acceptedFiles) {
+    acceptedFiles.forEach(file => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.setState({
+              fileAsDataURL: reader.result
+            });
+        };
+
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+
+        reader.readAsDataURL(file);
+    });
   }
 
   render() {
@@ -17,9 +34,16 @@ class ShareMemory extends Component {
       <div class="share-memory">
         <h1>Do you have any memories to share?</h1>
         <div class="container justify-content-center">
-          <div class="upload row">
+          <div class="upload row"> 
             <div class="col-auto">
-              <i class="far fa-file-image"></i>
+              <Dropzone
+                accept="image/jpeg, image/png"
+                onDrop={ (accepted) => { this._onDrop(accepted) } }
+              >
+                <div class="dropbox" style={ { 'backgroundImage': 'url(' + this.state.fileAsDataURL + ')' } }>
+                </div>
+                <i class="far fa-file-image"></i>
+              </Dropzone>
             </div>
             <div class="col">
               <p class="align-middle">If you want to share one of your memories with your fellow TravelPosters, drag & drop an image into this area.</p>
